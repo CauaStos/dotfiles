8f32805aed7567dcac151feb7627da35ea6b82aa
CauaStos <caua.oli.santos@gmail.com>
2024-10-28 03:50:29 -0300

    Updating dotfiles

---
+++ Documents_Folder/Scripts/screenshot_hyprlock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hyprshot -m output
+hyprlock
---
+++ Documents_Folder/Scripts/update_dots_repo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#Tutorial INSANO pro rsync aqui tá
+#
+#
+#-a: Archive mode, which preserves permissions, timestamps, and recursively copies directories.
+#-v: Verbose, shows progress.
+#--exclude: Specifies each folder or file to exclude from the copy. 
+#--include: Specifies each folder or file to include from the specified path.
+#
+#
+
+
+#cd into the dotfiles directory so the git status --porcelain doesnt return nothing
+cd ~/Documents/dotfiles/ || exit
+
+#Document Folders Copy
+include=(--include 'Hyprlock Assets/' --include 'Scripts/' --include 'Wallpapers/' --exclude '*/')
+
+echo "Copying document folders..."
+
+rsync -av "${include[@]}" ~/Documents/ ~/Documents/dotfiles/Documents_Folder/
+
+#.config Folders Copy
+
+echo "Copying .config folders..."
+
+include=(--include 'hypr/' --include 'waybar/' --include 'qt5ct/' --include 'qt6ct/' --include 'kitty/' --exclude '*/')
+
+rsync -av "${include[@]}" ~/.config/ ~/Documents/dotfiles/.config
+
+rm -f ~/Documents/dotfiles/.config/*
+rm -f ~/Documents/dotfiles/.config/.*
+
+if [[ $(git status --porcelain) ]]; then
+    echo "pushing changes"
+    
+    git add .
+    git commit -m "Updating dotfiles"
+    git push origin main
+else 
+    echo "no changes to commit"
+fi
+
+echo "Done!"
+exit 0
