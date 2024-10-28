890172ed2f10b2583b68659ae27e1470053a8dcc
CauaStos <caua.oli.santos@gmail.com>
2024-10-28 03:49:28 -0300

    Updating dotfiles

--- Documents/Scripts/update_dots_repo.sh
+++ Documents/Scripts/update_dots_repo.sh
@@ -16,9 +16,13 @@ cd ~/Documents/dotfiles/ || exit
 #Document Folders Copy
 include=(--include 'Hyprlock Assets/' --include 'Scripts/' --include 'Wallpapers/' --exclude '*/')
 
+echo "Copying document folders..."
+
 rsync -av "${include[@]}" ~/Documents/ ~/Documents/dotfiles/Documents/
 
 #.config Folders Copy
+
+echo "Copying .config folders..."
 
 include=(--include 'hypr/' --include 'waybar/' --include 'qt5ct/' --include 'qt6ct/' --include 'kitty/' --exclude '*/')
 
@@ -28,10 +32,11 @@ rm -f ~/Documents/dotfiles/.config/*
 rm -f ~/Documents/dotfiles/.config/.*
 
 if [[ $(git status --porcelain) ]]; then
+    echo "pushing changes"
+    
     git add .
     git commit -m "Updating dotfiles"
     git push origin main
-
 else 
     echo "no changes to commit"
 fi
