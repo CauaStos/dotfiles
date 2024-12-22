8bcf75a3f9627600e980c2603a81060522209e4f
CauaStos <caua.oli.santos@gmail.com>
2024-12-22 03:48:38 -0300

    Updating dotfiles

--- Documents/Scripts/update_dots_repo.sh
+++ Documents/Scripts/update_dots_repo.sh
@@ -25,7 +25,7 @@ rsync -av --checksum --mkpath "${include[@]}" ~/Documents/ ~/Documents/dotfiles/
 
 echo "Copying .config folders..."
 
-include=(--include 'hypr/' --include 'waybar/' --include 'foot/' --include 'macchina/' --include 'macchina/themes/' --include 'qt5ct/' --include 'qt5ct/colors/' --include 'qt6ct/' --include 'qt6ct/colors/' --exclude '*/')
+include=(--include 'hypr/' --include 'foot/' --include 'macchina/' --include 'macchina/themes/' --include 'qt5ct/' --include 'qt5ct/colors/' --include 'qt6ct/' --include 'qt6ct/colors/' --exclude '*/')
 
 rsync -av --checksum --mkpath "${include[@]}" ~/.config/ ~/Documents/dotfiles/.config
 
