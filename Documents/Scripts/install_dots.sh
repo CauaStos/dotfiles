b6401914b52eef8a63ec3105e7d3ffe0b3d67259
CauaStos <caua.oli.santos@gmail.com>
2024-12-26 03:54:35 -0300

    Updating dotfiles

--- Documents/Scripts/install_dots.sh
+++ Documents/Scripts/install_dots.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-#Sending folders to home
-rsync -av --checksum ./Documents ~/
+#installing packages that are used by config. (Also installing AUR Helper of my preference, YAY)
+
+#yay install
+
+echo "Installing AUR Helper (YAY)"
+
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. #we 'cd ..' since we are inside of yay-bin
+
+#arch packages installation
+
+echo "Installing necessary packages (pacman)"
+
+sudo pacman -S hyprland sddm polkit-gnome hyprpaper hypridle spotify-launcher nwg-look hyprlock foot zed thunar hyprpicker hyprlauncher
+
+#AUR packages installation
+
+echo "Installing necessary packages (AUR)"
+
+yay -S ags-hyprpanel-git hyprsunset zen-browser-bin spotify vesktop opentabletdriver catppuccin-gtk-theme-mocha catppuccin-cursors-mocha mission-center hyprshot
+
+
+#Copying folders to home
+
+echo "Moving files to home. '~/'"
+rsync -av --checksum ./Documents ./.config ~/
+
+echo "Done!"
+exit 0
--- install_dots.sh
+++ install_dots.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-#installing packages that are used. (Installing YAY too)
-
+#installing packages that are used by config. (Also installing AUR Helper of my preference, YAY)
 
 #yay install
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+echo "Installing AUR Helper (YAY)"
+
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. #we 'cd ..' since we are inside of yay-bin
 
 #arch packages installation
-sudo pacman -S polkit-gnome hyprpaper hypridle spotify-launcher
+
+echo "Installing necessary packages (pacman)"
+
+sudo pacman -S hyprland sddm polkit-gnome hyprpaper hypridle spotify-launcher nwg-look hyprlock foot zed thunar hyprpicker hyprlauncher
 
 #AUR packages installation
 
-yay -S ags-hyprpanel-git hyprsunset zen-browser-bin spotify vesktop opentabletdriver
+echo "Installing necessary packages (AUR)"
+
+yay -S ags-hyprpanel-git hyprsunset zen-browser-bin spotify vesktop opentabletdriver catppuccin-gtk-theme-mocha catppuccin-cursors-mocha mission-center hyprshot
 
 
 #Copying folders to home
+
+echo "Moving files to home. '~/'"
 rsync -av --checksum ./Documents ./.config ~/
+
+echo "Done!"
+exit 0
