4a06e4308175250af4704f9ced58c6f6059e28b1
Cauã de Oliveira Santos <98362674+CauaStos@users.noreply.github.com>
2024-12-26 20:03:02 -0300

    Update install_dots.sh

--- Documents/Scripts/install_dots.sh
+++ Documents/Scripts/install_dots.sh
@@ -6,19 +6,19 @@
 
 echo "Installing AUR Helper (YAY)"
 
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. #we 'cd ..' since we are inside of yay-bin
+sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. #we 'cd ..' since we are inside of yay-bin
 
 #arch packages installation
 
 echo "Installing necessary packages (pacman)"
 
-sudo pacman -S hyprland sddm polkit-gnome hyprpaper hypridle spotify-launcher nwg-look hyprlock foot zed thunar hyprpicker hyprlauncher papirus-icon-theme macchina neofetch zoxide starship pipewire pipewire-pulse pipewire-audio pipewire-alsa zsh
+sudo pacman -S --noconfirm hyprland sddm polkit-gnome hyprpaper hypridle spotify-launcher nwg-look hyprlock foot zed thunar hyprpicker papirus-icon-theme macchina neofetch zoxide starship pipewire pipewire-pulse pipewire-audio pipewire-alsa zsh
 
 #AUR packages installation
 
 echo "Installing necessary packages (AUR)"
 
-yay -S ags-hyprpanel-git hyprsunset zen-browser-bin spotify vesktop opentabletdriver catppuccin-gtk-theme-mocha catppuccin-cursors-mocha mission-center hyprshot
+yay -S --noconfirm ags-hyprpanel-git hyprsunset zen-browser-bin spotify vesktop opentabletdriver catppuccin-gtk-theme-mocha catppuccin-cursors-mocha mission-center hyprshot
 
 #Installing Catppuccin Icons
 echo "Installing Catppuccin Papirus icons (Mocha flavor, Mauve accent)"
@@ -51,11 +51,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 #Copying folders to home
 
 echo "Moving files to home. '~/'"
-rsync -av --checksum ./Documents ./.config ./home/.zshrc ~/
+rsync -av --checksum --mkpath ./Documents ./.config ./home/.zshrc ~/
 
 echo "Moving specific files."
 
-rsync -av --checksum ./.local/share/zed/extensions/ ~/.local/share/zed/extensions/
+rsync -av --checksum --mkpath ./.local/share/zed/extensions/ ~/.local/share/zed/extensions/
 
 #Enabling Services
 
