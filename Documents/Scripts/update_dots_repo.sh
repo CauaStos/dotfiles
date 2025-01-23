866c7191d2434b88043946e18e62ac7f111afa8f
CauaStos <caua.oli.santos@gmail.com>
2025-01-23 23:38:38 +0000

    Updating dotfiles

--- Documents/Scripts/update_dots_repo.sh
+++ Documents/Scripts/update_dots_repo.sh
@@ -28,7 +28,7 @@ rsync -av --checksum --mkpath ~/.zshrc ~/Documents/dotfiles/home/
 
 rsync -av --checksum --mkpath ~/.local/share/zed/extensions/ ~/Documents/dotfiles/.local/share/zed/extensions
 
-rsync -av --checksum --mkpath ~/.local/share/themes/Material ~/Documents/dotfiles/home/
+rsync -av --checksum --mkpath ~/.local/share/themes/Material ~/Documents/dotfiles/home/.local/share/themes/
 
 
 
