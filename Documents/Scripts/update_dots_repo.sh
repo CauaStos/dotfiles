c3c71be8e01ff34e183a8c2a1a5b6ea714c797e4
CauaStos <caua.oli.santos@gmail.com>
2025-01-28 05:25:05 -0300

    Updating dotfiles

--- Documents/Scripts/update_dots_repo.sh
+++ Documents/Scripts/update_dots_repo.sh
@@ -36,7 +36,7 @@ rsync -av --checksum --mkpath ~/.local/share/themes/Material ~/Documents/dotfile
 
 echo "Copying '.config' folders..."
 
-include=(--include 'hypr/' --include 'macchina/' --include 'macchina/themes/' --include 'qt5ct/' --include 'qt5ct/colors/' --include 'qt6ct/' --include 'qt6ct/colors/' --include 'ags/' --include 'zed/' --include 'matugen/' --include 'matugen/templates/' --exclude '*/')
+include=(--include 'hypr/' --include 'macchina/' --include 'macchina/themes/' --include 'qt5ct/' --include 'qt5ct/colors/' --include 'qt6ct/' --include 'qt6ct/colors/' --include 'ags/*' --include 'zed/' --include 'matugen/' --include 'matugen/templates/' --exclude '*/')
 
 rsync -av --checksum --mkpath "${include[@]}" ~/.config/ ~/Documents/dotfiles/.config
 
