24a53aeeca51f9fbc234b00bac5ae5a188900ae4
CauaStos <caua.oli.santos@gmail.com>
2025-02-09 02:08:10 -0300

    Updating dotfiles

--- .config/ags/widgets/Test.tsx
+++
@@ -1,9 +0,0 @@
-import { Window } from "astal/gtk3/widget";
-
-export default function Test(monitor = 0) {
-  return (
-    <Window monitor={monitor} visible>
-      <box>Content of the widget</box>
-    </Window>
-  );
-}
