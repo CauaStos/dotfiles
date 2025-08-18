6be2f6af48962f4406107e07cadda2e0ed13e540
CauaStos <caua.oli.santos@gmail.com>
2025-08-18 15:34:01 -0300

    add json lsp

--- dot-config/nvim/lua/configs/lspconfig.lua
+++ dot-config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "dartls", "lua_ls", "wgsl_analyzer" }
+local servers = { "html", "cssls", "dartls", "lua_ls", "wgsl_analyzer", "jsonls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
